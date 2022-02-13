184dd705cf8fa9404f5faf67960478441622402c
Jeferson Salazar <jeferson.salazar1016@gmail.com>
2022-02-13 18:34:15 -0500

    Se crean disenios para mostrar lista de peliculas

--- domain/src/main/java/com/jeferson/chiper/android/moviedb/domain/MovieDomain.kt
+++ domain/src/main/java/com/jeferson/chiper/android/moviedb/domain/MovieDomain.kt
@@ -1,4 +1,18 @@
 package com.jeferson.chiper.android.moviedb.domain
 
-class MovieDomain {
-}
+data class MovieDomain(
+    val adult: Boolean,
+    val backdrop_path: String?,
+    val genre_ids: List<Int>?,
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String?,
+    val popularity: Double,
+    val poster_path: String?,
+    val release_date: String?,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Float?,
+    val vote_count: Int,
+)
