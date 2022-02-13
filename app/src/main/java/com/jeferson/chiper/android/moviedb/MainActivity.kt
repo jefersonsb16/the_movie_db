ef97fde744526735f3a03d9017fd9dc7459603e3
Jeferson Salazar <jeferson.salazar1016@gmail.com>
2022-02-13 15:22:06 -0500

    Se guardan archivos iniciales al crear el proyecto

---
+++ app/src/main/java/com/jeferson/chiper/android/moviedb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jeferson.chiper.android.moviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
